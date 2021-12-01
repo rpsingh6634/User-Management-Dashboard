3c62ddaa9dcc7dac59a656ed8e8526daf8d3f81c
rpsingh6634 <rpsingh6634@gmail.com>
2021-12-01 12:14:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let projectRow = document.getElementsByClassName("table-row");
+const projectData = projectRow.children;
+let size = projectData.length;
+const deleteRow = document.getElementsByClassName("deleteRow");
+
+
+
+/* Updating projact detail  */
+
+
+
+for (let i = 0; i < deleteRow.length; i++) {
+      console.log(deleteRow[i].addEventListener('click',
+            function () {
+                  projectRow[i].remove();
+                  updateProjectData()
+            }
+      ))
+};
